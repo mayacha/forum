4a0499e90aab0ef629b36f8b93f6edde255afd7f
mayacha <maria.chabab@gmail.com>
2015-09-01 12:54:55 +0200

    User Manager V1

--- models/UserManager.class.php
+++ models/UserManager.class.php
@@ -7,32 +7,62 @@ class UserManager
 	{
 		$this->link = $link;
 	}
-	public function create()
+	public function create($login,$email,$password,$avatar,$date,$description)
 	{
-
+		$request="INSERT INTO user (login,email,password,avatar,date,description) VALUES ('".$login."','".$email."','".$password."','".$avatar."','".$date."','".$description."')";	
+		$res= mysqli_query($this->link, $request);
+			if ($res === false)
+			{
+				$error="une erreur est survenue";
+				return $error;
+			}
+			else
+			{
+				$success="Bienvenu parmis nous !";
+				return $success;
+			}
 	}
-	public function delete()
+	public function delete($id)
 	{
-
+		$request="UPDATE user SET id_permission='4' WHERE id='".$id."'";
+		$res= mysqli_query($this->link, $request);
+			if ($res === false)
+			{
+				$error="une erreur est survenue";
+				return $error;
+			}
+			else
+			{
+				$success="Ce compte a bien été désactivé";
+				return $success;
+			}
 	}
-	public function update($link, $id, $email, $password, $name, $firstname){
-	$request = "UPDATE user SET name='".$name."', firstname='".$firstname."', email='".$email."', password='".$password."' WHERE user.id = ".$id.";";
-	$res = mysqli_query($this->link, $request);
-	if ($res === false){
-		$message = mysqli_error($link);
-		$code = mysqli_errno($link);
-		if($code == 1062){
-			$prob = substr($message, -6, -1);
-			if($prob == "email"){
-				return "email";
-			}else{
-				return "error";
+	public function update($id, $email, $password, $login, $description, $avatar)
+	{
+		$request = "UPDATE user SET name='".$name."', firstname='".$firstname."', email='".$email."', password='".$password."' WHERE user.id = ".$id.";";
+		$res = mysqli_query($this->link, $request);
+		if ($res === false){
+			$message = mysqli_error($this->link);
+			$code = mysqli_errno($this->link);
+			if($code == 1062){
+				$prob = substr($message, -6, -1);
+				if($prob == "email"){
+					$error="email non valide";
+					return $error;
+				}
+				else
+				{
+					$error="une erreur est survenue";
+					return $error;
+				}
 			}
 		}
-	}else{	
-		return "success";
+		else
+		{	
+			$success="Vos modifications ont bien été prises en compte";
+			return $success;
+		}
 	}
-}
 	public function selectById($id)
 	{
 		$request = "SELECT * FROM user WHERE id='".$id."'";
