28d909110f2fd6b85365b5656b036974ae12a028
remigit <sarzerojer@gmail.com>
2015-09-01 16:00:21 +0200

    admin

--- public/scripts/script-admin.js
+++ public/scripts/script-admin.js
@@ -1,12 +1,16 @@
+/*** gestion catégories ***/
+// bouton modifier
 $('.btn-update-category').on("click", function(){
 	$(this).parent().parent().find('.js-category-form-modif').toggle("slow");
 });
-
+// bouton supprimer
 $('.btn-delete-category').on("click", function(){
 	$(this).parent().parent().find('.js-category-form-delete').toggle("slow");
 });
-
+// bouton annuler suppression
 $('.btn-cancel-delete-category').on("click", function(event){
     event.preventDefault();
 	$(this).parent().parent().toggle("slow");
 });
+
+/*** gestion utilisateurs ***/ 
