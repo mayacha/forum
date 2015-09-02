51d605dc370d3bb5d39d8eaac75e05526725584d
mayacha <maria.chabab@gmail.com>
2015-09-02 13:47:38 +0200

    register à peu près fonctionnel

--- apps/traitement_register.php
+++ apps/traitement_register.php
@@ -1,26 +1,35 @@
 <?php
 $login='';
 $email='';
-$birthdate='';
-require('models/UserManager.class.php');
+
 $manager = new UserManager($link);
 
-if(isset($_POST['login'],$_POST['email'],$_POST['birthdate'],$_POST['password'],$_POST['check-password']))
+if(isset($_POST['login'],$_POST['email'],$_POST['password'],$_POST['check-password']))
 {
-	if($_POST['password']==$_POST['checkpassword'])
+	if($_POST['password']==$_POST['check-password'])
 	{
 		try
 		{
-			$user = $manager->create($_POST);
+			$user= new User($link);
+			$user->setLogin($_POST['login']);
+			$user->setEmail($_POST['email']);
+			$user->setPassword($_POST['password']);
+			$res= $manager->create($user);
+			// header('Location:home');
+			echo $res;
 		}
 		catch (Exception $exception)
 		{
+			$login=$_POST['login'];
+			$email=$_POST['email'];
 			$error = $exception->getMessage();
 		}
 	}
 	else
 	{
+		$login=$_POST['login'];
+		$email=$_POST['email'];
 		$error="les mots de passe saisis ne correspondent pas";
 	}
-}	
+}
 ?>
