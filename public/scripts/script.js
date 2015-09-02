c89e67c465f4622bdb3e279972157c3c5f775a52
mayacha <maria.chabab@gmail.com>
2015-09-02 15:33:59 +0200

    maj maria

---
+++ public/scripts/script.js
@@ -0,0 +1,51 @@
+
+   // connexion AJAX
+    function connexionAJAX(){
+        $.post('login',
+        { email: $("#email").val(), password: $("#password").val()},
+        function(data){
+            if(data == "reload"){
+                window.location.reload();
+            }else{
+                $('.dropdown-module').html(data);
+                addEventConnexionAJAX();
+            }
+        });
+    };
+    function addEventConnexionAJAX(){
+        $(".btn-form-login").on('click', function(event){
+            event.preventDefault();
+            connexionAJAX();
+        });
+        $(".js-loginform").on('submit', function(event){
+            event.preventDefault();
+            connexionAJAX();
+        });
+    };
+    addEventConnexionAJAX();
+
+//formulaires profil
+$('#changelog').click(function(e){
+        e.preventDefault();
+        $('#login').slideToggle(200);
+    });
+    $('#changemail').click(function(e){
+        e.preventDefault();
+        $('#email').slideToggle(200);
+    });
+    $('#changemdp').click(function(e){
+        e.preventDefault();
+        $('#mdp').slideToggle(200);
+    });
+    $('#changedate').click(function(e){
+        e.preventDefault();
+        $('#birth').slideToggle(200);
+    });
+
+
+
+
+
+});
+
+
