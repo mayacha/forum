169534661882f133bbe83966fdb270e33d3b5d4d
JeanGithub <jeanpuyrinier@gmail.com>
2015-09-03 11:54:57 +0200

    lancer 3

--- apps/display-listPosts.php
+++ apps/display-listPosts.php
@@ -1,11 +1,9 @@
 <?php
-$listPosts=new Topic($link);
+$topic=new Topic($link);
 $listPosts=$topic->selectAll();
 foreach($listPosts as $post)
 {
-	$id_author=$post->getIduser();
-	// $content=$post->getContent();
-	// $titre=$post->getTitle();
+
 	require('views/listPosts.phtml');
 }
 
