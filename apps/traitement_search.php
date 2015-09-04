c81940c0299fc7492e839e888718a03bc0e161a8
mayacha <maria.chabab@gmail.com>
2015-09-04 15:26:34 +0200

    recherche en cours...

--- apps/traitement_search.php
+++ apps/traitement_search.php
@@ -1,26 +1,43 @@
 <?php
-$catManager = new CategoryManager($link);
-$topManager =	new TopicManager($link);
+$catManager = new Category($link);
+$topManager =	new Topic($link);
 if (isset($_GET['search']) && $_GET['search']!=="")
 {
 	$search = $_GET['search'];
 
 	if(isset($_Get['category'])&& !isset($_Get['topic']))
 	{
+		try
+		{
 		$category_name=$_GET['category'];
-		$category=$catManager->selectByName($category_name);
+		$catManager->selectByName($category_name);
 		$id_category=$category->getId();
-
-		$result=$catManager->selectCatTopics($id_category);
-		
+		$catManager->searchCatTopics($id_category);
+		}
+		catch(Exception $e)
+		{
+			$error=$e->getMessage();
+		}
 	}
 	if(isset($_Get['topic'], $_Get['category']))
 	{
-
+		try
+		{
+		$topic_name=$_GET['topic'];
+		$topManager->selectByName($category_name);
+		$id_category=$category->getId();
+		$topManager->searchTopicPosts($id_topic);
+		}
+		catch(Exception $e)
+		{
+			$error=$e->getMessage();
+		}
 	}
 	else
 	{
 
+		$catManager->searchAllTopics();
+		$topManager->searchAllPosts();
 	}
 }
 ?>
