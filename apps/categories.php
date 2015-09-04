6836a8423b0a70ddec812126e19589db694ba882
JeanGithub <jeanpuyrinier@gmail.com>
2015-09-04 09:19:03 +0200

    ajout sur 1er MAJ vendredi

---
+++ apps/categories.php
@@ -0,0 +1,12 @@
+<?php 
+
+$categoryManager=new CategoryManager($link);
+$listcategories=$categoryManager->selectAll();
+foreach($listcategories as $category)
+{
+	require('views/listCat.phtml');
+}
+
+
+
+?>
