34c70eb6cb64e33a96c554571895c551e5a3ba54
JeanGithub <jeanpuyrinier@gmail.com>
2015-09-04 09:17:36 +0200

    1er MAJ vendredi

--- apps/display-SinglelistTopics.php
+++ apps/display-SinglelistTopics.php
@@ -1,6 +1,5 @@
 <?php
-$manager = new CategoryManager($link);
-$category = $manager->selectByName($_GET['category']);
+
 $topic = $category->selectByName($_GET['topic']);
 $listposts = $topic->selectAll();
 
--- apps/display-listPosts.php
+++ apps/display-listPosts.php
@@ -1,12 +1,12 @@
-<?php
-$topic=new Topic($link);
-$listPosts=$topic->selectAll();
-foreach($listPosts as $post)
-{
+// <?php
+// $topic=new Topic($link);
+// $listPosts=$topic->selectAll();
+// foreach($listPosts as $post)
+// {
 
-	require('views/listPosts.phtml');
-}
+// 	require('views/listPosts.phtml');
+// }
 
 
 
-?>
+// ?>
