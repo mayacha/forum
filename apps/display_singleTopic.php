7bd420b2381796d242424120bed48118ea427a90
JeanGithub <jeanpuyrinier@gmail.com>
2015-09-04 13:21:38 +0200

    der

--- apps/display_singleTopic.php
+++
@@ -1,9 +0,0 @@
-// <?php
-// $singleTopic= new Topic($link);
-// $singleTopic=$topic->select($id);
-// $TopicName=$topic->getName();
-// $author=$topic->getAuthor();
-// require('views/singleTopic.phtml');
-
-
-// ?>
