7302d2c3fb239aa23f550e6ee9eec34190a684c8
JeanGithub <jeanpuyrinier@gmail.com>
2015-09-08 15:48:43 +0200

    maj html-css avec des nougats dedans

--- public/scripts/editscript.js
+++ public/scripts/editscript.js
@@ -9,8 +9,14 @@ var button=$('.js-button').on('click', function (event)
 	
 });
 
-var buttontopic=$('.js-button-topic').on('click', function(event))
+var buttontopic=$('.js-button-topic').on('click', function (event)
 {
 	event.preventDefault();
 	var change=$(this).find('+div').toggle();
-}
+});
+
+var buttonpost=$('.js-button-create').on('click', function (event)
+{
+	event.preventDefault();
+	var change=$(this).find('+div').toggle();
+});
